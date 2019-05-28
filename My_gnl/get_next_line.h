c44017ff0ab3b2a816f51bf417bb35aec864fa4b
Nikolay Nikitin <nikitin.na@phystech.edu>
2019-05-28 18:36:22 +0300

    Creation of my git repository

---
+++ My_gnl/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crycherd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/22 22:18:24 by crycherd          #+#    #+#             */
+/*   Updated: 2019/05/23 18:18:20 by crycherd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# define BUFF_SIZE 1000000
+
+int	get_next_line(const int fd, char **line);
+
+#endif
